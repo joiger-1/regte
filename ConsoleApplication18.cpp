2bcf736bdb0c93f5911ae309ad126bf26ba066f6
joiger-1 <78557697+joiger-1@users.noreply.github.com>
2021-02-07 20:51:11 +0300

    Add files via upload

---
+++ ConsoleApplication18.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> arr(n);
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	bool a;
+	int maxi = 1;
+	int lasta = arr[0], lastb = -1;
+
+	for (int i = 1; i < n; i++) {
+		if (arr[i] == lasta) {
+			if (arr[i] != lastb)
+				maxi++;
+			lastb = arr[i];
+		}
+		else
+		{
+			lasta = arr[i];
+			maxi++;
+		}
+	}
+	cout << maxi;
+}
